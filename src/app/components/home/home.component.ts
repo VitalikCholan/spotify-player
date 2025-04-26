7f6669231805f6f57debb2c10dc7197bace113d3
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2025-04-26 12:10:53 +0300

    Fix issue with logout functionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,9 +9,10 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
   logout() {
     this.auth.logout();
+    this.router.navigate(['/login']); // Redirect to login after logout
   }
 }
